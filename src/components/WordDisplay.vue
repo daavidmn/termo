4390a17179bd90417cf7fb9efc3e67d9320d017e
David Macedo <david.fund@gmail.com>
2022-05-16 10:31:40 -0300

    basic game logic added

--- src/components/WordDisplay.vue
+++ src/components/WordDisplay.vue
@@ -1,12 +1,39 @@
 <template>
-  <div id="word-container">
-    <div v-for="(char, index) in characters" :key="index">
-      <div
-        class="letter-box cursor-pointer"
-        :class="index == charSelected ? `selected` : ``"
-        @click="selectIndex(index)"
-      >
-        {{ char }}
+  <div class="word-list">
+    <div class="word-container">
+      <div v-for="(char, index) in playerChars" :key="index">
+        <div
+          class="letter-box cursor-pointer"
+          :class="index == charSelected ? `selected` : ``"
+          @click="selectIndex(index)"
+        >
+          {{ char }}
+        </div>
+      </div>
+    </div>
+    <div
+      v-for="(change, chanceIndex) in chances"
+      :key="chanceIndex"
+      class="word-container"
+    >
+      <div v-for="(char, charIndex) in chars" :key="charIndex">
+        <div
+          v-if="wordList[chanceIndex]"
+          class="letter-box"
+          :class="
+            !hasChar(wordList[chanceIndex][charIndex])
+              ? `false-char`
+              : hasRightPositionChar(
+                  wordList[chanceIndex][charIndex],
+                  charIndex
+                )
+              ? `correct-char`
+              : `unpositioned-char`
+          "
+        >
+          {{ wordList[chanceIndex][charIndex] }}
+        </div>
+        <div v-else class="letter-box"></div>
       </div>
     </div>
   </div>
@@ -19,11 +46,16 @@ export default {
   name: "WordDisplay",
   data() {
     return {
-      characters: ["a", "b", "c", "d", "e"],
+      chars: 5,
+      chances: 5,
+      playerChars: ["a", "b", "c", "d", "e"],
+      rightWord: [],
       charSelected: 0,
+      wordList: [],
     };
   },
   beforeMount() {
+    this.rightWord = this.getDailyWord.split("");
     window.addEventListener("keypress", this.listemKeyPress);
   },
   beforeDestroy() {
@@ -45,45 +77,75 @@ export default {
       return regex.test(char);
     },
     checkAnswer() {
-      if (this.characters.join("") === this.getDailyWord) {
+      if (this.playerChars.join("") === this.getDailyWord) {
+        window.removeEventListener("keypress", this.listemKeyPress);
         alert("acertou!");
-      } else alert("errou!");
-
-      console.log(this.characters.join(""), this.getDailyWord);
+      } else {
+        this.wordList.push([...this.playerChars]);
+        alert("errou!");
+      }
     },
     listemKeyPress(event) {
       const char = String.fromCharCode(event.keyCode);
       if (this.validateKeyPressed(char)) {
-        this.characters[this.charSelected] = char;
-        this.characters = [...this.characters];
+        this.playerChars[this.charSelected] = char;
+        this.playerChars = [...this.playerChars];
         this.charSelected < 4 ? this.charSelected++ : (this.charSelected = 0);
       }
 
       if (event.keyCode === 13) this.checkAnswer();
+    },
+    hasChar(char) {
+      return this.rightWord.some((rightChar) => rightChar === char);
+    },
+    hasRightPositionChar(char, charIndex) {
+      return this.getAllIndexes(this.rightWord, char).includes(charIndex);
+    },
+    getAllIndexes(arr, val) {
+      let indexes = [];
+      for (let i = 0; i < arr.length; i++) if (arr[i] === val) indexes.push(i);
+      return indexes;
     },
   },
 };
 </script>
 
 <style scoped lang="scss">
-#word-container {
+.word-list {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   gap: 5px;
-  .letter-box {
+  .word-container {
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 50px;
-    height: 50px;
-    border: 2px solid black;
-    border-radius: 5px;
-    font-size: 1.6rem;
-    text-transform: uppercase;
-  }
-  .selected {
-    text-decoration: underline;
+    flex-direction: row;
+    gap: 5px;
+    .letter-box {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 50px;
+      height: 50px;
+      border: 2px solid black;
+      border-radius: 5px;
+      font-size: 1.6rem;
+      text-transform: uppercase;
+    }
+    .false-char {
+      background-color: grey;
+    }
+    .correct-char {
+      background-color: green;
+    }
+    .unpositioned-char {
+      background-color: yellow;
+    }
+    .selected {
+      text-decoration: underline;
+    }
   }
 }
 </style>
